bc3f22391a88ca78b551d58db8d0844e7354c07a
Parker Ballner <pballner@gmail.com>
2017-09-04 21:06:33 -0400

    Finished About Me component, added some more logos for extracirriculars

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'about-me',
     templateUrl: './about-me.component.html',
     styleUrls: ['./about-me.component.styles.css']
 })
-export class AboutMeComponent {};
+export class AboutMeComponent { };
